b2884ef0dae9d270c065a598282effe237124b3e
Sota Nakai <soooota1201@gmail.com>
2020-07-11 13:48:58 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+$(function() {
+  $('a[href^="#"]').click(function() {
+    let speed = 400;
+
+    let href = $(this).attr("href");
+
+    let target = $(href == "#" || href == "" ? 'html' : href);
+
+    let position = target.offset().top;
+
+    $('body,html').animate({ scrollTop: position }, speed, 'swing');
+    return false;
+  });
+});
